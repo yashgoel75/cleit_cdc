ca2e4d6ce7616bfbe432d83c26096b52a3ab5534
yashgoel <yash.goel8370@gmail.com>
2025-08-16 11:59:09 +0530

    fix: checks in registration form

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -231,9 +231,12 @@ export default function Society() {
   }
   async function sendEmailOtp() {
     try {
-      // if (falseEmailFormat) {
-      //   return;
-      // }
+      if (falseEmailFormat) {
+        return;
+      }
+      if (isEmailEmpty) {
+        return;
+      }
       setCheckingMail(true);
       const res = await fetch(
         `/api/register/user?email=${formData.collegeEmail}`
@@ -340,6 +343,7 @@ export default function Society() {
       setIsConfirmPasswordEmpty(formData.confirmPassword == "");
       return;
     }
+    if (!validOtp) return;
     setIsBasicDetails(false);
     setIsAcademicDetails(true);
   }
@@ -585,7 +589,7 @@ export default function Society() {
                       >
                         <path d="M480-280q17 0 28.5-11.5T520-320q0-17-11.5-28.5T480-360q-17 0-28.5 11.5T440-320q0 17 11.5 28.5T480-280Zm-40-160h80v-240h-80v240Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
                       </svg>
-                      &nbsp; Please enter society username
+                      &nbsp; Please enter username
                     </div>
                   ) : null}
                 </div>
@@ -686,7 +690,9 @@ export default function Society() {
                       {otpSending
                         ? "Sending"
                         : checkingMail
-                        ? "Checking Mail"
+                        ? isMobile
+                          ? "Checking"
+                          : "Checking Mail"
                         : otpSent
                         ? "Sent"
                         : "Send OTP"}
